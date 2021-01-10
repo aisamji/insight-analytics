1b1142966bfe59ac81516b7e8464e6339e3a2f92
Ali I Samji <ali.samji@outlook.com>
2021-01-10 15:58:28 -0600

    Cleaned up functional test.

--- tests/test_campaign_analytics.py
+++ tests/test_campaign_analytics.py
@@ -4,21 +4,21 @@ import mock_sheet
 app.mailchimp3 = mockchimp3
 app.build = mock_sheet.build
 
+mock_spreadsheet = mock_sheet.GSheetValues()
+master_sheet = mock_spreadsheet._sheets['master']
+test1_sheet = mock_spreadsheet._sheets['test1']
+open_rate = mockchimp3._reports['opens']['open_rate']
+click_rate = mockchimp3._reports['clicks']['click_rate']
+urls_clicked = mockchimp3._click_details['urls_clicked']
 
-def test_add_report(capsys):
+
+def test_add_report():
     sheet_id = 'apple-3fd'
-    mock_spreadsheet = mock_sheet.GSheetValues()
-    master_sheet = mock_spreadsheet._sheets['master']
-    test1_sheet = mock_spreadsheet._sheets['test1']
-    transposed = list(zip(*master_sheet))
-    open_rate = mockchimp3._reports['opens']['open_rate']
-    click_rate = mockchimp3._reports['clicks']['click_rate']
-    urls_clicked = mockchimp3._click_details['urls_clicked']
 
     app.main(['Test1', '--to-google-sheet', sheet_id])
 
     assert (
-        'Test1' in transposed[8][10:16]
+        'Test1' in transposed_master()[8][10:16]
         and open_rate == test1_sheet[1][1]
         and click_rate == test1_sheet[2][1]
         and _is_equal(urls_clicked[0], test1_sheet[5])
@@ -32,3 +32,7 @@ def _is_equal(url_clicked_info, sheet_row_data):
         and url_clicked_info['click_percentage'] == sheet_row_data[3]
         and url_clicked_info['unique_click_percentage'] == sheet_row_data[4]
     )
+
+
+def transposed_master():
+    return list(zip(*master_sheet))
