30a33fdde3467af7cb36895e7e4ed591a18c76da
Ali I Samji <ali.samji@outlook.com>
2021-01-10 17:30:29 -0600

    Fixed some bugs.

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 import sys
 import argparse
 import os
+from datetime import datetime
 
 from mailchimp3 import MailChimp
 from googleapiclient.discovery import build
@@ -11,7 +12,7 @@ from google.oauth2.service_account import Credentials
 def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('campaign_name')
-    parser.add_argument('--to-google-sheet', '-gs', dest='google_sheet')
+    parser.add_argument('--to-google-sheet', '-gs', dest='google_sheet', required=True)
     ns = parser.parse_args(args)
 
     mc_client = MailChimp(mc_api='45ef254b0660a24cf859d541e5d1ddd4-us16')
@@ -28,37 +29,43 @@ def main(args):
     append_body = {
         'values': [[ns.campaign_name]]
     }
+
     gsheet_client.spreadsheets().values().append(
         spreadsheetId=ns.google_sheet,
         range='Master!I11:I16',
         body=append_body,
+        valueInputOption='RAW'
     ).execute()
 
     update_ranges = [f'{ns.campaign_name}!B2:B3', f'{ns.campaign_name}!C6:E22']
     mc_report = mc_client.reports.get(campaign_id=mc_campaign_id)
     mc_click_details = mc_client.reports.click_details.all(campaign_id=mc_campaign_id)
     urls_clicked = mc_click_details['urls_clicked']
-    update_body = {'data': [
-        {
-            'range': f'{ns.campaign_name}!B2:B3',
-            'values': [[
-                mc_report['opens']['open_rate'],
-                mc_report['clicks']['click_rate']
-            ]],
-            'majorDimension': 'COLUMNS',
-        },
-        {
-            'range': f'{ns.campaign_name}!C6:E22',
-            'values': [
-                [info['url'], info['click_percentage'], info['unique_click_percentage']]
-                for info in urls_clicked
-            ]
-        }
-    ]}
+    update_body = {
+        'valueInputOption': 'RAW',
+        'data': [
+            {
+                'range': f'{ns.campaign_name}!B1:B3',
+                'values': [[
+                    datetime.now().strftime('%B %-d, %Y at %-I:%M %p'),
+                    mc_report['opens']['open_rate'],
+                    mc_report['clicks']['click_rate']
+                ]],
+                'majorDimension': 'COLUMNS',
+            },
+            {
+                'range': f'{ns.campaign_name}!C6:E22',
+                'values': [
+                    [info['url'], info['click_percentage'], info['unique_click_percentage']]
+                    for info in urls_clicked
+                ]
+            }
+        ]
+    }
 
     gsheet_client.spreadsheets().values().batchUpdate(
         spreadsheetId=ns.google_sheet,
-        body=update_body,
+        body=update_body
     ).execute()
 
 
